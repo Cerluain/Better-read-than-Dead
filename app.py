bfe83abad2a2ee6ec5ba7bbdf5cfafea13dc0b26
Cerluain <leonardo@masinialeman.com>
2023-04-13 19:32:49 -0700

    FIXED ISSUE OF MODULES

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
+import sys
+sys.path.append("c:/users/leona/appdata/local/programs/python/python39/lib/site-packages")
+
 from flask import Flask, render_template, url_for
-app = Flask(__name__)
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+
 
 @app.route("/")
 @app.route("/home")
@@ -8,3 +14,11 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
